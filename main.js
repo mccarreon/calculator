b12912e31e6d6382101dd6f694b78fe708d08ef7
Matt Carreon <mattcarryon@gmail.com>
2020-08-05 14:45:40 -0700

    finished functionality

--- main.js
+++ main.js
@@ -1,56 +1,70 @@
 const buttons = [
     {
         id: 'clear',
-        content: 'C'
+        content: 'C',
+        func: clearDisplay
     },
     {
         id: 'negative',
-        content: '±'
+        content: '±',
+        func: switchSign
     },
     {
-        id: 'percent',
-        content: '%'
+        id: 'backspace',
+        content: '←',
+        func: backspace
     },
     {
         id: 'divide',
         content: '÷',
-        class: 'operator'
+        class: 'operator',
+        func: selectOperator
     },
     {
         id: 'multiply',
         content: '×',
-        class: 'operator'
+        class: 'operator',
+        func: selectOperator
     },
     {
         id: 'subtract',
         content: '-',
-        class: 'operator'
+        class: 'operator',
+        func: selectOperator
     },
     {
         id: 'add',
         content: '+',
-        class: 'operator'
+        class: 'operator',
+        func: selectOperator
     },
     {
         id: 'equals',
         content: '=',
-        class: 'operator'
+        class: 'operator',
+        func: operate
     },
     {
         id: 'decimal',
-        content: '.'
+        content: '.',
+        func: updateDisplay
     },
 ];
+const display = document.getElementById("display");
 const num_order = [7,8,9,
                    4,5,6,
                    1,2,3,
                    0]
+let display_value = 0;
+let first_number = 0;
+let operator = '';
 
 num_order.forEach((i) => {
     let btn = {
         id: 'num'+i,
         content: i,
-        class: "number"
+        class: "number",
+        func: updateDisplay
     }
     buttons.push(btn);
 });
@@ -61,9 +75,9 @@ buttons.forEach(btn => {
     node.setAttribute("id", btn.id);
     if (btn.class) {
         node.classList.add(btn.class);
-        if (btn.class == "number") {
-            node.addEventListener('click', updateDisplay);
-        }
+    }
+    if (btn.func) {
+        node.addEventListener('click', btn.func);
     }
     node.classList.add("btn");
     node.innerHTML = btn.content
@@ -86,18 +100,63 @@ let divide = (x, y) => {
     return x / y;
 };
 
-let operate = (op, x, y) => {
-    return op(x,y);
+function operate () {
+    if (operator == '+') {
+        display.innerHTML = add(first_number, display_value);
+    } else if (operator == '-') {
+        display.innerHTML = subtract(first_number, display_value);
+    } else if (operator == '-') {
+        display.innerHTML = subtract(first_number, display_value);
+    } else if (operator == '×') {
+        display.innerHTML = multiply(first_number, display_value);
+    } else if (operator == '÷') {
+        display.innerHTML = divide(first_number, display_value);
+    }
 };
 
+function checkDisplayForZero(){
+    if (display.innerHTML == '0'){
+        display.innerHTML = '';
+    }
+}
+
 function updateDisplay(){
-    let display = document.getElementById("display");
-    display.innerHTML += this.innerHTML;
-    display_value = display.innerHTML;
+    if (display.innerHTML.length < 10) {
+        if (this.innerHTML == '.' && !display.innerHTML.includes('.')) {
+            display.innerHTML += this.innerHTML;
+            display_value = Number(display.innerHTML);
+        } else if (this.innerHTML != '.') {
+            checkDisplayForZero()
+
+            display.innerHTML += this.innerHTML;
+            display_value = Number(display.innerHTML);
+        }
+    }
+}
+
+function clearDisplay(){
+    display.innerHTML = '0';
+}
+
+function switchSign(){
+    display.innerHTML = display_value*-1;
+    display_value = Number(display.innerHTML);
+}
+
+function backspace(){
+    display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length-1)
+    if (display.innerHTML == '' || display.innerHTML == '-') {
+        display.innerHTML = 0;
+    }
+}
+
+function selectOperator(){
+    operator = this.innerHTML;
+    first_number = display_value;
+    display.innerHTML = '0';
 }
 /**
  * TODO:
- *  - add decimal checking
  *  - add javascript to buttons
  *  - add number selection storage
  *  - make it look good
